e7ae1b2575e3ee878ccd7eac13578af06c49e114
Bảo Nguyễn <baosmile7743285@gmail.com>
2019-11-05 10:17:27 +0700

    Updates

---
+++ precache-manifest.deb65ad38fbb6e06871dbf10cf993ef3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba785fc846ada71214876d9fd3660bd5",
+    "url": "/monster-relodex/index.html"
+  },
+  {
+    "revision": "f68c6834439d0dea2932",
+    "url": "/monster-relodex/static/css/main.6a0a2bfa.chunk.css"
+  },
+  {
+    "revision": "07bcf9f5f05f07401e44",
+    "url": "/monster-relodex/static/js/2.07a6689f.chunk.js"
+  },
+  {
+    "revision": "f68c6834439d0dea2932",
+    "url": "/monster-relodex/static/js/main.1a451b35.chunk.js"
+  },
+  {
+    "revision": "e730fa188ac414f0d105",
+    "url": "/monster-relodex/static/js/runtime-main.4be2b03e.js"
+  }
+]);
