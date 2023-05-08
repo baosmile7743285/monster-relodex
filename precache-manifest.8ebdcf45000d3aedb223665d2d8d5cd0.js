63c0af5fe476b42e8753e0039225f4c466c69758
baosmile7743285 <baosmile7743285>
2023-05-08 23:07:16 +0700

    Updates

---
+++ precache-manifest.8ebdcf45000d3aedb223665d2d8d5cd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1efa37429ce8a04763c4f39dd0ebacc",
+    "url": "/monster-relodex/index.html"
+  },
+  {
+    "revision": "1878698dc7d1a64d2f48",
+    "url": "/monster-relodex/static/css/main.782e001e.chunk.css"
+  },
+  {
+    "revision": "e7af9e2ce3926a761dfc",
+    "url": "/monster-relodex/static/js/2.f8565693.chunk.js"
+  },
+  {
+    "revision": "1878698dc7d1a64d2f48",
+    "url": "/monster-relodex/static/js/main.bffbfc93.chunk.js"
+  },
+  {
+    "revision": "d327104d449a9cdbab05",
+    "url": "/monster-relodex/static/js/runtime-main.6e5f27fd.js"
+  }
+]);
